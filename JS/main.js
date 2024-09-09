55b3346dcf5dbd9cb95ccd38bd8b2bb83e20a71d
stephany <stephany@teste.com>
2024-09-09 15:12:23 -0300

    commit inicial

---
+++ JS/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('#carrossel-imagens').slick({
+        autoplay:true,
+    })
+
+    $('.menu-hamburger').click(function(){
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome : 'Por favor, insira seu nome',
+            telefone: 'Por favor, insira seu telefone com DDD',
+            email: 'Por favor, insira seu e-mail',
+            mensagem: 'Este campo é obrigatório'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            console.log(camposIncorretos)
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function() {
+        const destino = $('#contato');
+        const nomeVeiculo =$(this).parent().find('h3').text();
+
+        $('#veiculoDeInteresse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
